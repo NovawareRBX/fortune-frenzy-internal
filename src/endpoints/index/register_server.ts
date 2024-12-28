c3a8d2a40acd9edf5f56ed35b42965fffa6a1f1c
Noxirity <frazersearle@outlook.com>
2024-12-28 20:31:12 +0000

    Add IP address to server registration in database

--- src/endpoints/index/register_server.ts
+++ src/endpoints/index/register_server.ts
@@ -7,12 +7,13 @@ export default async function (request: FastifyRequest<{ Params: { server_id: st
 	const server_id = request.params.server_id;
 	const maria = await getMariaConnection();
 	const redis = await getRedisConnection();
+	const ip_address = request.headers["cf-connecting-ip"]
 
 	if (!maria || !redis) {
 		return [500, { error: "Failed to connect to the database" }];
 	}
 
-	await maria.query("INSERT INTO active_roblox_servers (id) VALUES (?)", [server_id]);
+	await maria.query("INSERT INTO active_roblox_servers (id, ip_address) VALUES (?)", [server_id, ip_address]);
 
 	const initial_api_key = randomBytes(32).toString("hex");
 	await redis.set(`api_key:${server_id}`, createHash("sha256").update(initial_api_key).digest("hex"), {
